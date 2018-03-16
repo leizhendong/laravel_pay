9734c0313944ff00dd90aceff6eab247b0df931d
搞事Boy <lzdong@foxmail.com>
2018-03-16 22:46:36 +0800

    index

--- public/index.php
+++ public/index.php
@@ -37,17 +37,7 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
